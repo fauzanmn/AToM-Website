c0b4ce345c1f5fcb470647c63096b74b3c773b8c
allblue <100438762+fauzanmn@users.noreply.github.com>
2024-01-07 01:01:14 +0700

    Delete

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Variabel database
-    // Membuat koneksi ke Database
-    $konek = mysqli_connect("localhost", "root", "", "dbawos");
-	if(mysqli_connect_error()){ // mengecek apakah koneksi database error
-		echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error(); // pesan ketika koneksi database error
-	}
-?>
